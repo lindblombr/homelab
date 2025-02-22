96bce116e031fd3778392504f8950797f602790f
Brian Lindblom <lindblombr@gmail.com>
2025-02-21 22:43:12 -0500

    Deploy kueue via flux

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -4,4 +4,4 @@ flux install
 
 flux create secret git homelab --url=ssh://git@github.com/lindblombr/homelab --ssh-key-algorithm=ecdsa --ssh-ecdsa-curve=p521
 flux create source git homelab --url=ssh://git@github.com/lindblombr/homelab --branch main --secret-ref homelab
-flux create kustomization homelab --source=GitRepository/homelab --path=./manifests --prune=true --interval=5m
+flux create kustomization homelab-stage1 --source=GitRepository/homelab --path=./stage1 --prune=true --interval=5m
