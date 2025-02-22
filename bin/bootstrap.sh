cebb4355af13d20dd1be3d5656894dcbfba7ab61
Brian Lindblom <lindblombr@gmail.com>
2025-02-21 22:25:49 -0500

    Set up kustomize stages

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -2,7 +2,6 @@
 
 flux install
 
-flux create source git homelab --url=https://github.com/lindblombr/homelab --branch main
-flux create secret git homelab --url=ssh://git@github.com/lindblombr/homelab --ssh-key-algorithm=ecdsa \
-  --ssh-ecdsa-curve=p521
+flux create secret git homelab --url=ssh://git@github.com/lindblombr/homelab --ssh-key-algorithm=ecdsa --ssh-ecdsa-curve=p521
+flux create source git homelab --url=ssh://git@github.com/lindblombr/homelab --branch main --secret-ref homelab
 flux create kustomization homelab --source=GitRepository/homelab --path=./manifests --prune=true --interval=5m
